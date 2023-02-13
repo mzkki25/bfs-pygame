d5dafc09b497ece17ab341841369b12732df7e31
Akmal <103729910+akmalmzkki@users.noreply.github.com>
2023-02-13 23:03:06 +0700

    Update bfs.py

--- bfs.py
+++ bfs.py
@@ -2,24 +2,26 @@ import pygame as pg
 from collections import deque
 from random import random
 
-
+# Fungsi untuk mendapatkan rect dari grid
 def get_grid_rect(x, y):
     return x * TILE + 1, y * TILE + 1, TILE - 2, TILE - 2
 
+# Fungsi untuk mendapatkan node yang bersebelahan
 def get_adjacent_nodes(x, y):
     within_bounds = lambda x, y: 0 <= x < cols and 0 <= y < rows
     walkable = lambda x, y: not grid[y][x]
     move_options = [[-1, 0], [0, -1], [1, 0], [0, 1], [-1, -1], [1, -1], [1, 1], [-1, 1]]
     return [(x + dx, y + dy) for dx, dy in move_options if within_bounds(x + dx, y + dy) and walkable(x + dx, y + dy)]
 
+# Fungsi untuk mendapatkan node yang diklik
 def get_clicked_node():
     mouse_pos = pg.mouse.get_pos()
     grid_x, grid_y = mouse_pos[0] // TILE, mouse_pos[1] // TILE
-    pg.draw.rect(screen, pg.Color('red'), get_grid_rect(grid_x, grid_y))
+    pg.draw.rect(screen, pg.Color('pink'), get_grid_rect(grid_x, grid_y))
     mouse_clicks = pg.mouse.get_pressed()
     return (grid_x, grid_y) if mouse_clicks[0] else False
 
-
+# Fungsi BFS
 def breadth_first_search(start, goal, graph):
     q = deque([start])
     visited = {start: None}
@@ -39,6 +41,7 @@ if __name__ == '__main__':
     cols, rows = 25, 20
     TILE = 30
 
+    # Inisialisasi pygame
     pg.init()
     screen = pg.display.set_mode([cols * TILE, rows * TILE])
     clock = pg.time.Clock()
@@ -54,18 +57,19 @@ if __name__ == '__main__':
     bfs_queue, visited = deque([start]), {start: None}
 
     while True:
-        screen.fill(pg.Color('black'))
+        screen.fill(pg.Color('darkslategray'))
 
         [[pg.draw.rect(screen, pg.Color('darkorange'), get_grid_rect(x, y), border_radius=TILE // 5)
           for x, col in enumerate(row) if col] for y, row in enumerate(grid)]
 
         [pg.draw.rect(screen, pg.Color("forestgreen"), get_grid_rect(x, y)) for x, y in visited]
-        [pg.draw.rect(screen, pg.Color("darkslategray"), get_grid_rect(x, y)) for x, y in bfs_queue]
+        [pg.draw.rect(screen, pg.Color("lightgreen"), get_grid_rect(x, y)) for x, y in bfs_queue]
+
 
         # Melakukan BFS dan mendapatkan path dari mouse click
         mouse_pos = get_clicked_node()
         if mouse_pos and not grid[mouse_pos[1]][mouse_pos[0]]:
-            queue, visited = breadth_first_search(start, mouse_pos, graph)
+            bfs_queue, visited = breadth_first_search(start, mouse_pos, graph)
             goal = mouse_pos
 
         # Gambar path
